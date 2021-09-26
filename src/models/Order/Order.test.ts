18635c1380247b7b782af56da885f7271c5ee0f4
André Seiji Tamanaha <andre.seiji@loft.com.br>
2021-09-26 14:21:12 -0300

    test: add 3 items placed order case

--- src/models/Order/Order.test.ts
+++ src/models/Order/Order.test.ts
@@ -22,4 +22,20 @@ describe('Given Order', () => {
       }).toThrow(new InvalidCpfError());
     });
   });
+
+  describe('When 3 items are added', () => {
+    test('it should place order with 3 items', () => {
+      const cpf = '935.411.347-80';
+      const items: Array<OrderItem> = [];
+      const order = new Order(items);
+      const apple = new OrderItem('apple', 2, 1);
+      const banana = new OrderItem('banana', 1.50, 1);
+      const carrot = new OrderItem('carrot', 1.0, 1);
+      order.addItem(apple);
+      order.addItem(banana);
+      order.addItem(carrot);
+      const placedOrder = order.placeOrder(cpf);
+      expect(placedOrder.items).toHaveLength(3);
+    });
+  });
 });
